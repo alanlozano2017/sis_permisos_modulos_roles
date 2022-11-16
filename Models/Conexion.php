c0749e1099ddc5d1f1b59e135d1aa0a2b249656b
Alan Lozano <alan.yo.99@gmail.com>
2022-11-16 16:15:52 -0300

    cambio de base de datos en Conexion php provisoria

--- Models/Conexion.php
+++ Models/Conexion.php
@@ -3,7 +3,7 @@
 Class Conexion{
 
     public static function conectar(){
-        $servidor = "localhost"; $usuario = "root"; $contrasena = ""; $nombreBaseDatos = "rrhh";
+        $servidor = "localhost"; $usuario = "root"; $contrasena = ""; $nombreBaseDatos = "db_s1";
         $conexionBD = new mysqli($servidor, $usuario, $contrasena, $nombreBaseDatos);
         if ($conexionBD->connect_error) {
             die("Connection failed: " . $conn->connect_error);
